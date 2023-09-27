78dfbd0b8da1829d72e491b38542d017c32821d7
KlintonICT <chhunklinton91@gmail.com>
2023-09-27 16:02:22 +0700

    create indexed db cache content

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,6 @@
-var CACHE_STATIC_NAME = 'static-v13';
+importScripts('/src/js/idb.js');
+
+var CACHE_STATIC_NAME = 'static-v16';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
   '/',
@@ -6,6 +8,7 @@ var STATIC_FILES = [
   '/offline.html',
   '/src/js/app.js',
   '/src/js/feed.js',
+  '/src/js/idb.js',
   '/src/js/promise.js',
   '/src/js/fetch.js',
   '/src/js/material.min.js',
@@ -16,6 +19,12 @@ var STATIC_FILES = [
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
 ];
+
+var dbPromise = idb.open('posts-store', 1, function (db) {
+  if (!db.objectStoreNames.contains('posts')) {
+    db.createObjectStore('posts', { keyPath: 'id' });
+  }
+});
 
 // * This is how we trim cache
 // function trimCache(cacheName, maxItems) {
@@ -68,15 +77,24 @@ function isInArray(string, array) {
 }
 
 self.addEventListener('fetch', function (event) {
-  var url = 'https://httpbin.org/get';
+  var url =
+    'https://pwa-simple-ig-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json';
   if (event.request.url.indexOf(url) > -1) {
     event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
-        return fetch(event.request).then(function (res) {
-          // trimCache(CACHE_DYNAMIC_NAME, 3);
-          cache.put(event.request, res.clone());
-          return res;
+      fetch(event.request).then(function (res) {
+        const cloneRes = res.clone();
+        cloneRes.json().then((data) => {
+          for (let key in data) {
+            dbPromise.then((db) => {
+              var tx = db.transaction('posts', 'readwrite');
+              var store = tx.objectStore('posts');
+              console.log(data[key]);
+              store.put(data[key]);
+              return tx.complete;
+            });
+          }
         });
+        return res;
       })
     );
   } else if (isInArray(event.request.url, STATIC_FILES)) {
